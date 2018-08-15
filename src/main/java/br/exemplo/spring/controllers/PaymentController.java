5a56d27fd4292b306262d3b28d5d363874bb7b21
Lincoln Pires <lincoln.ufop@gmail.com>
2018-08-15 09:15:21 -0300

    Integracao com aplicacao de pagamento

--- src/main/java/br/exemplo/spring/controllers/PaymentController.java
+++ src/main/java/br/exemplo/spring/controllers/PaymentController.java
@@ -7,8 +7,12 @@ import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.client.HttpClientErrorException;
+import org.springframework.web.client.RestClientException;
+import org.springframework.web.client.RestTemplate;
 import org.springframework.web.context.WebApplicationContext;
 
+import br.exemplo.spring.models.PaymentData;
 import br.exemplo.spring.models.ShoppingCart;
 
 @Controller
@@ -18,12 +22,25 @@ public class PaymentController {
 
 	@Autowired
 	private ShoppingCart shoppingCart;
+	@Autowired
+	private RestTemplate restTemplate;
+	
 
 	@RequestMapping(value = "checkout", method = RequestMethod.POST, name = "checkoutPaymentController")
 	public String checkout() {
 		BigDecimal total = shoppingCart.getTotal();
-		//codigo de integração
 		
-		return "redirect:/success";
+		//codigo de integração com a aplicação web chamada book-payment, hospedada atualmente no Heroku.
+		//só aceita pagamentos de até 500 reais e os dados devem ser enviados no formato Json
+		String uriToPay = "http://book-payment.herokuapp.com/payment";
+		
+		try {
+			String response = restTemplate.postForObject(uriToPay, new PaymentData(total), String.class);
+			return "payment/success";
+		} catch (HttpClientErrorException e) {
+			e.printStackTrace();
+			return "payment/error";
+		}
+		
 	}
 }
