cd661a0c3251a280e997c69b29f3f9d2103ef446
Lincoln Pires <lincoln.ufop@gmail.com>
2018-08-16 08:37:32 -0300

    Conf. filtro do Spring Security - Adicionado necessidade de autenticacao

---
+++ src/main/java/br/exemplo/spring/conf/SpringSecurityFilterConfiguration.java
@@ -0,0 +1,7 @@
+package br.exemplo.spring.conf;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+public class SpringSecurityFilterConfiguration extends AbstractSecurityWebApplicationInitializer {
+
+}
