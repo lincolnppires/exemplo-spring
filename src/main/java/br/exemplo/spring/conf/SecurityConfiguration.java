e258b7b483a8cc9ff467abec4e56ec53defc82f1
Lincoln Pires <lincoln.ufop@gmail.com>
2018-08-16 09:09:04 -0300

    Customizando as regras de acesso

--- src/main/java/br/exemplo/spring/conf/SecurityConfiguration.java
+++ src/main/java/br/exemplo/spring/conf/SecurityConfiguration.java
@@ -1,9 +1,22 @@
 package br.exemplo.spring.conf;
 
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @EnableWebSecurity
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+	
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.authorizeRequests()
+			.antMatchers("/produtos/form").hasRole("ADMIN")
+			.antMatchers("/shopping/**").permitAll()
+			.antMatchers(HttpMethod.POST, "/produtos").hasRole("ADMIN")
+			.antMatchers("/produtos/**").permitAll()
+			.anyRequest().authenticated()
+			.and().formLogin();
+	}
 
 }
