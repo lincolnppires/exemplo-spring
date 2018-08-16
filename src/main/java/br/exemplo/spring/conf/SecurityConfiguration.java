13eb7868485fdda54a5a2e0f283d90f6e332ad3a
Lincoln Pires <lincoln.ufop@gmail.com>
2018-08-16 15:08:17 -0300

    Habilitando o token do CSRF

--- src/main/java/br/exemplo/spring/conf/SecurityConfiguration.java
+++ src/main/java/br/exemplo/spring/conf/SecurityConfiguration.java
@@ -4,12 +4,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.annotation.web.servlet.configuration.EnableWebMvcSecurity;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
-@EnableWebSecurity
+@SuppressWarnings("deprecation")
+@EnableWebMvcSecurity
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 	
 	@Override
