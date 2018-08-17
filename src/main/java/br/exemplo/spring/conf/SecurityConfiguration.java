551ace82c304ffc3f75bcae9e0b087004e83c092
Lincoln Pires <lincoln.ufop@gmail.com>
2018-08-17 08:08:14 -0300

    Habilitando logout atraves do metodo GET

--- src/main/java/br/exemplo/spring/conf/SecurityConfiguration.java
+++ src/main/java/br/exemplo/spring/conf/SecurityConfiguration.java
@@ -8,6 +8,8 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.config.annotation.web.servlet.configuration.EnableWebMvcSecurity;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+import org.springframework.util.AntPathMatcher;
 
 @SuppressWarnings("deprecation")
 @EnableWebMvcSecurity
@@ -21,7 +23,9 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 			.antMatchers(HttpMethod.POST, "/produtos").hasRole("ADMIN")
 			.antMatchers("/produtos/**").permitAll()
 			.anyRequest().authenticated()
-			.and().formLogin();
+			.and().formLogin()
+			.and()
+			.logout().logoutRequestMatcher(new AntPathRequestMatcher("/logout"));
 	}
 	
 	@Autowired
